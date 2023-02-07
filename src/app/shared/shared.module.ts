77c24582c215c10f06aed9e516b2e3683ac4aee0
Ahmeeed22 <78164396+Ahmeeed22@users.noreply.github.com>
2023-02-07 08:43:41 +0200

    add feature

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ButtonModule} from 'primeng/button';
 import {DropdownModule} from 'primeng/dropdown';
-<<<<<<< HEAD
-// primeng components
-import { InputTextModule } from 'primeng/inputtext';
-import { CardModule } from 'primeng/card';
-import { InputTextareaModule } from "primeng/inputtextarea";
-=======
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 
->>>>>>> aa4385f2596b37774ec6afcd564cbe83d912f0fb
 
 @NgModule({
   declarations: [
@@ -22,20 +15,12 @@ import { RouterModule } from '@angular/router';
     RouterModule,
     ButtonModule,
     DropdownModule,
-    InputTextModule,
-    CardModule,
-    InputTextareaModule
+ 
   ],
   exports:[
     ButtonModule,
     DropdownModule,
-<<<<<<< HEAD
-    InputTextModule,
-    CardModule,
-    InputTextareaModule
-=======
     NavbarComponent
->>>>>>> aa4385f2596b37774ec6afcd564cbe83d912f0fb
 
   ]
 })
