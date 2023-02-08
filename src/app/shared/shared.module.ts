abb58d5c7fdf0c6729a6948764764b3ac77fd9e5
Ahmeeed22 <78164396+Ahmeeed22@users.noreply.github.com>
2023-02-08 21:25:22 +0400

    add step three inhance

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,10 @@ import {ButtonModule} from 'primeng/button';
 import {DropdownModule} from 'primeng/dropdown';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import {DragDropModule} from 'primeng/dragdrop';
+import { TableModule } from "primeng/table";
+import { ToastModule } from "primeng/toast";
+import { CardModule } from "primeng/card";
 
 
 @NgModule({
@@ -15,12 +19,20 @@ import { RouterModule } from '@angular/router';
     RouterModule,
     ButtonModule,
     DropdownModule,
+    DragDropModule,
+TableModule,
+ToastModule,
+CardModule
  
   ],
   exports:[
     ButtonModule,
     DropdownModule,
-    NavbarComponent
+    NavbarComponent,
+    DragDropModule,
+    TableModule,
+    ToastModule,
+    CardModule
 
   ]
 })
