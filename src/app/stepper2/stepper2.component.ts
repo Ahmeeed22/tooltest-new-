bffb85fcaa7053bc5775ee932b945db50fd51baa
Ahmeeed22 <78164396+Ahmeeed22@users.noreply.github.com>
2023-03-07 21:52:37 +0400

    export to exel

--- src/app/stepper2/stepper2.component.ts
+++ src/app/stepper2/stepper2.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit ,ElementRef ,AfterViewInit} from '@angular/core';
+import { Component, OnInit ,ElementRef ,AfterViewInit, ViewChild} from '@angular/core';
 import {FormBuilder, Validators} from '@angular/forms';
 import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
 import {MatStepper, StepperOrientation} from '@angular/material/stepper';
@@ -8,6 +8,8 @@ import {BreakpointObserver} from '@angular/cdk/layout';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { AuthService } from '../auth/services/auth.service';
+import * as XLSX from "xlsx";
+
 export interface User {
   name: string;
   id?:Number
@@ -25,6 +27,7 @@ export interface User {
 })
 
 export class Stepper2Component implements OnInit ,AfterViewInit {
+  @ViewChild("table") table!: ElementRef;
   wizarFinalData:any={}
   finalData :any ;
   firstFormGroup :any ;
@@ -169,13 +172,14 @@ example:any;
     console.log(this.finalData);
     this._AuthService.dealingAi(this.finalData).subscribe({
       next :(res)=>{
-        console.log(res);
+        console.log('this.finalData',this.finalData);
         console.log(res.data.toString());
         console.log(res.data);
         this.result=res.data
         this.flagTable=!this.flagTable
         this._AuthService.addwizard(this.wizarFinalData).subscribe({
           next: (res)=>{
+            console.log('this.wizarFinalData',this.wizarFinalData);
           },
           error : (err)=>{
             console.log(err);
@@ -191,4 +195,37 @@ example:any;
     localStorage.removeItem("project_title");
     localStorage.removeItem("project_description")
   }
+
+fireEvent() {
+  console.log( this.table);
+  console.log(this.table.nativeElement);
+  const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(
+    this.table.nativeElement
+  );
+
+  /* new format */
+  var fmt = "0.00";
+  /* change cell format of range B2:D4 */
+  // var range = { s: { r: 1, c: 1 }, e: { r: 2, c: 100000 } };
+  // for (var R = range.s.r; R <= range.e.r; ++R) {
+  //   for (var C = range.s.c; C <= range.e.c; ++C) {
+  //     var cell = ws[XLSX.utils.encode_cell({ r: R, c: C })];
+  //     if (!cell || cell.t != "n") continue; // only format numeric cells
+  //     cell.z = fmt;
+  //   }
+  // }
+  const wb: XLSX.WorkBook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
+  var fmt = "@";
+  wb.Sheets["Sheet1"]["F"] = fmt;
+    console.log(this.wizarFinalData.test_cases[this.wizarFinalData.test_cases.length-1].name);
+    console.log(this.wizarFinalData.test_cases[this.wizarFinalData.test_cases.length-1]);
+    console.log(this.wizarFinalData.test_cases);
+    
+  /* save to file */
+  XLSX.writeFile(wb, `_export_ ${this.wizarFinalData.test_cases[this.wizarFinalData.test_cases.length-1].name}.xlsx`);
+}
+
+
+ 
 }
